2cc36c1ec031ed5b229032636722619961510f5d
Syed Kounain Abbas Rizvi <55267488+abbasmashaddy72@users.noreply.github.com>
2022-08-01 01:21:31 +0530

    Initial commit

---
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\ContactUs;
+use App\Models\Review;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $contactedUs = ContactUs::count();
+        $blogsCount = Blog::count();
+        $blogsViewSum = Blog::sum('clicks');
+        $reviewsCount = Review::count();
+
+        return view('pages.backend.dashboard', compact([
+            'contactedUs',
+            'blogsCount',
+            'blogsViewSum',
+            'reviewsCount'
+        ]));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// 'index', 'create', 'store', 'show', 'edit', 'update', 'destroy'
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth', 'namespace' => 'App\Http\Controllers\Backend'], function () {
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+    Route::get('user/permissions/{id}', 'UserController@permission_edit')->name('user.permissions.edit');
+
+    Route::post('user/permissions/{id}', 'UserController@permission_save')->name('user.permissions.save');
+
+    Route::resource('user', 'UserController')->only([
+        'index', 'create', 'store',  'edit', 'show'
+    ]);
+
+    Route::get('homepage', 'HomePageController@index')->name('homepage');
+
+    Route::resource('contact-us', 'ContactUsController')->only([
+        'index', 'show'
+    ]);
+
+    Route::get('about-us', 'AboutUsController@index')->name('aboutUs');
+
+    Route::resource('faq', 'FaqController')->only([
+        'index', 'create', 'store',  'edit'
+    ]);
+
+    Route::resource('review', 'ReviewController')->only([
+        'index', 'create', 'store',  'edit'
+    ]);
+
+    Route::resource('team', 'TeamController')->only([
+        'index', 'create', 'store',  'edit'
+    ]);
+
+    Route::post('image_upload', 'MiscellaneousController@image_upload')->name('ckeditor.upload');
+
+    Route::resource('blog', 'BlogController')->only([
+        'index', 'create', 'store',  'edit'
+    ]);
+
+    Route::resource('feature', 'FeatureController')->only([
+        'index', 'create', 'store',  'edit'
+    ]);
+
+    Route::group(['prefix' => 'laravel-filemanager', 'middleware' => 'auth'], function () {
+        \UniSharp\LaravelFilemanager\Lfm::routes();
+    });
+});
+
+require __DIR__ . '/auth.php';
